c96a839e680f16edb3dd36e0deec2b97fd27f453
Striker775 <76661847+Striker775@users.noreply.github.com>
2024-04-20 10:22:51 +0300

    Users updated

--- data/users.py
+++ data/users.py
@@ -8,9 +8,14 @@ class User(SqlAlchemyBase):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    email = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    patronymic = sqlalchemy.Column(sqlalchemy.String)
     login = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String)
+    level = sqlalchemy.Column(sqlalchemy.Integer)
     password = sqlalchemy.Column(sqlalchemy.String)
+
     
 
 
