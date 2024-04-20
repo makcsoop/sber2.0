e39808e333fe3f8128d0d9a462e0395fccb08064
Striker775 <76661847+Striker775@users.noreply.github.com>
2024-04-20 12:05:49 +0300

    Login Registration forms

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from flask_login import LoginManager, login_user
 from data import db_session
 from flask_wtf import FlaskForm
@@ -153,25 +153,52 @@ def generator_log():
 # @app.route('/')
 # def index():
 #     return render_template('index.html', obj=all_info)
-db_session.global_init('main.db')
+db_session.global_init('db/user.db')
 db_sess = db_session.create_session()
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login_form():
     form = LoginForm()
     if form.validate_on_submit():
-        login_cur = db_sess.query(User).filter(User.login == form.login.data).first()
+        login_cur = db_sess.query(User).filter(User.login == str(form.login.data)).first()
         if not login_cur:
-            return render_template('index.html', title='Авторизация',
+            return render_template('login_user.html', title='Авторизация',
                                    form=form,
                                    message="Такого пользователя не существует")
         elif login_cur.password != form.password.data:
-            return render_template('index.html', title='Авторизация',
+            return render_template('login_user.html', title='Авторизация',
                                    form=form,
                                    message="Неверный пароль")
+        session['login'] = form.login.data
+        return redirect('/')
+    return render_template('login_user.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register_form():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print(123)
+        db_sess = db_session.create_session()
+        login_cur = db_sess.query(User).filter(User.login == form.login.data).first()
+        if login_cur:
+            return render_template('register_new.html', title='Авторизация',
+                                   form=form,
+                                   message="Такой пользователь уже существует")
+        user = User()
+        user.name = form.name.data
+        user.surname = form.surname.data
+        user.patronymic = form.patronymic.data
+        user.login = form.login.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db_sess.add(user)
+        db_sess.commit()
+        print(user)
+        return redirect('/')
     return render_template('register_new.html', title='Авторизация', form=form)
 
 @app.route('/table-edit', methods=['GET', 'POST'])
